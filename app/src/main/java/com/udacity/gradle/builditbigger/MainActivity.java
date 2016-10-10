b8dffd90e1d48bcf214d16b1c14f82e07a342d59
Electron <devversitymail@gmail.com>
2016-10-11 01:02:31 +0530

    added progressbar

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,39 +15,26 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void tellJoke(View view) {
-
-//        joker joker = new joker();
-//        launch display joke activity from jokelib_android
-//        Intent intent = new Intent(this, DisplayJokeActivity.class);
-//        intent.putExtra("joke", joker.getJoke());
-//        startActivity(intent);
-//        Toast.makeText(this, joker.getJoke(), Toast.LENGTH_SHORT).show();
-
-        new AsyncTaskEndpoint().execute(this);
-    }
-
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,13 +1,18 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.wordpress.electron0zero.jokelib_android.DisplayJokeActivity;
 
 
 /**
@@ -18,12 +23,31 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+    ProgressBar progressBar = null;
+    public String joke_loaded = null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
+
+        // show joke on button click
+
+        Button button = (Button) root.findViewById(R.id.btn_tell_joke);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                tellJoke();
+            }
+        });
+
+        progressBar = (ProgressBar) root.findViewById(R.id.joke_progress_bar);
+        progressBar.setVisibility(View.GONE);
+
+        // TODO: 11-10-16 FIX THIS SO WE CAN SEE AD ON REAL DEVICE
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
@@ -32,5 +56,18 @@ public class MainActivityFragment extends Fragment {
                 .build();
         mAdView.loadAd(adRequest);
         return root;
+
+    }
+    public void tellJoke(){
+        new AsyncTaskEndpoint().execute(this);
+    }
+
+    public void launchDisplayJokeActivity(){
+        Context context = getActivity();
+        Intent intent = new Intent(context, DisplayJokeActivity.class);
+        intent.putExtra(DisplayJokeActivity.JOKE_KEY, joke_loaded);
+        //Toast.makeText(context, loadedJoke, Toast.LENGTH_LONG).show();
+        context.startActivity(intent);
+        progressBar.setVisibility(View.GONE);
     }
 }
